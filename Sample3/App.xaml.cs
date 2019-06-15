970b1e38c13a12e3cab355b428dbfd296a3e23ce
Devwarlt <devwarlt@loesoft.org>
2019-06-15 15:05:29 -0300

    Make a simple login view MVVM

---
+++ Sample3/App.xaml.cs
@@ -0,0 +1,8 @@
+namspace Sample3 {
+    public partial class App : Application {
+        public void Start () {
+            // MainPage = new MainPage();
+            MainPage = new Login ();
+        }
+    }
+}
